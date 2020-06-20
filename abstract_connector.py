3303b5b4579a50178ae27743eef69676399b710d
darknight <darknight0x63@gmail.com>
2020-06-20 22:21:25 +0200

    Create abstract class for database connector classes

---
+++ abstract_connector.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractConnector(ABC):
+
+    @abstractmethod
+    def create_table(self, name: str, values: list):
+        pass
+
+    @abstractmethod
+    def drop_table(self, name: str):
+        pass
+
+    @abstractmethod
+    def create(self, table_name: str, values: dict):
+        pass
+
+    @abstractmethod
+    def read(self, table_name: str, columns: list = None, conditions: list = []):
+        pass
+
+    @abstractmethod
+    def update(self, table_name: str, values: dict, conditions: list = []):
+        pass
+
+    @abstractmethod
+    def delete(self, table_name: str, conditions: list = []):
+        pass

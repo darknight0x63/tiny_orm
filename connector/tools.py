940d0f24485a2b9d51cf1c40095402a501368dcb
darknight <darknight0x63@gmail.com>
2020-07-03 00:01:38 +0200

    Add support for 'IN' and 'NOT IN' for SQL conditions translator

--- connector/tools.py
+++ connector/tools.py
@@ -5,12 +5,19 @@ def _conditions_to_sql(conditions: list) -> str:
         if type(cond) == str and cond in ["&", "|"]:
             cond_str += " %s" % "AND" if cond == '&' else "OR"
         elif len(cond) == 3:
-            cond_str += " %s%s%s" % (cond[0], cond[1], "%s")
-            _values.append(cond[2])
+            operand_1, operator, operand_2 = cond[0], cond[1], cond[2]
+            if operator.lower() == "in" or operator.lower() == "not in":
+                cond_str += " %s %s %s" % \
+                            (operand_1,
+                             operator,
+                             "( " + ", ".join([("'%s'" % it) if it is not None else "NULL" for it in operand_2]) + " )")
+            else:
+                cond_str += " %s %s %s" % (operand_1, operator, "%s")
+                _values.append(operand_2)
         else:
             raise ValueError("Invalid expression %s" % cond)
 
     if cond_str:
-        cond_str = "WHERE " + cond_str
+        cond_str = "WHERE" + cond_str
 
     return cond_str, _values
